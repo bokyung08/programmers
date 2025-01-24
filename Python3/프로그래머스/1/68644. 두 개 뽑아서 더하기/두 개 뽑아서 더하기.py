dba798e4f82482ba6280048f25f3d4474dde9262
보경 <77604744+bokyung08@users.noreply.github.com>
2025-01-24 16:52:08 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 10.28 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    answer = []
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            if i!=j:
+                a=numbers[i]+numbers[j]
+                if a not in answer:
+                    answer.append(a)
+    return sorted(answer)
