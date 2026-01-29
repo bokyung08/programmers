b08806a0ec5940885d1a909b07d4e9643e768a9f
보경 <77604744+bokyung08@users.noreply.github.com>
2026-01-30 01:09:19 +0900

    [level 1] Title: 숫자 짝꿍, Time: 1115.70 ms, Memory: 27.7 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -0,0 +1,18 @@
+def solution(X,Y):
+    result = ""
+    a=[0]*10
+    b=[0]*10
+    for i in X:
+        value=int(i)
+        a[value]+=1
+    for i in Y:
+        value=int(i)
+        b[value]+=1
+    for i in range(9,-1,-1):
+        value=str(i)*min(a[i],b[i])
+        result+=value
+    if (len(result)==0):
+        return '-1'
+    if result[0]=='0':
+        return '0'
+    return result
