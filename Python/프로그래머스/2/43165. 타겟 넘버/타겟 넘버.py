e43f835585ad79b4b486979c1efe349f74621000
보경 <77604744+bokyung08@users.noreply.github.com>
2026-09-15 00:15:48 +0900

    [level 2] Title: 타겟 넘버, Time: 122.91 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ Python/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,10 @@
+def solution(numbers, target):
+    def dfs(idx, total): 
+        if idx == len(numbers): 
+            if total == target: 
+                return 1
+            return 0
+        plus = dfs(idx +1, total + numbers[idx])
+        minus = dfs(idx +1, total - numbers[idx])
+        return plus + minus
+    return dfs(0,0)
